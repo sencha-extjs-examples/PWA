2e373c8b2a74e6546e89898a22e0ec97e43998e9
Don Griffin <dongryphon@users.noreply.github.com>
2017-04-27 17:34:01 -0500

    Update MainController.js

--- client/app/view/main/MainController.js
+++ client/app/view/main/MainController.js
@@ -1,8 +1,6 @@
 /**
  * This class is the controller for the main view for the application. It is specified as
  * the "controller" of the Main view class.
- *
- * TODO - Replace this content of this view to suite the needs of your application.
  */
 Ext.define('PWA.view.main.MainController', {
     extend: 'Ext.app.ViewController',
@@ -27,8 +25,6 @@ Ext.define('PWA.view.main.MainController', {
     initViewModel: function() {
         var vm = this.getViewModel();
 
-        // add a way to listen for these in the "listen" object
-
         window.addEventListener("online", function() {
             vm.set('online', true);
             vm.getStore('personnel').reload();
@@ -48,17 +44,22 @@ Ext.define('PWA.view.main.MainController', {
     },
 
     showPerson: function(person) {
-        person = Ext.create('PWA.model.Person', person);
         var main = this.lookup('main');
         var view = this.lookup('person');
         var vm = view.getViewModel();
+
+        person = Ext.create('PWA.model.Person', person);
+
         vm.set({ record: person });
+
         vm.notify();
+
         main.setActiveItem(view);
     },
 
     showList: function() {
         var main = this.lookup('main');
+
         main.setActiveItem(0);
     }
 });
