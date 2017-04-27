c288166885624cc93b21add4d060855e5a0711d5
Don Griffin <dongryphon@users.noreply.github.com>
2017-04-27 17:46:37 -0500

    Delete Bindable.js

--- client/overrides/Bindable.js
+++
@@ -1,9 +0,0 @@
-!function() {
-    var old = Ext.mixin.Bindable.prototype.initBindable;
-
-    Ext.mixin.Bindable.prototype = function() {
-        var result = old.apply(this, arguments);
-        console.log('bound');
-        return result;
-    }
-}();
