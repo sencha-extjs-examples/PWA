6ea6cd2b97e978e95e7cc03ce27a49528fcf484b
Don Griffin <dongryphon@users.noreply.github.com>
2017-04-27 17:14:54 -0500

    Update app.js

--- client/app.js
+++ client/app.js
@@ -1,7 +1,6 @@
 /*
- * This file is generated and updated by Sencha Cmd. You can edit this file as
- * needed for your application, but these edits will have to be merged by
- * Sencha Cmd when upgrading.
+ * This file launches the application by asking Ext JS to create
+ * and launch() the Application class.
  */
 Ext.application({
     name: 'PWA',
@@ -9,18 +8,11 @@ Ext.application({
     extend: 'PWA.Application',
 
     requires: [
-        'PWA.view.main.Main'
+        'PWA.*'
     ],
 
-    // The name of the initial view to create. With the classic toolkit this class
-    // will gain a "viewport" plugin if it does not extend Ext.Viewport. With the
-    // modern toolkit, the main view will be added to the Viewport.
+    // The name of the initial view to create. The main view will
+    // be added to the Ext.Viewport.
     //
-    mainView: 'PWA.view.main.Main',
-
-    //-------------------------------------------------------------------------
-    // Most customizations should be made to PWA.Application. If you need to
-    // customize this file, doing so below this section reduces the likelihood
-    // of merge conflicts when upgrading to new versions of Sencha Cmd.
-    //-------------------------------------------------------------------------
+    mainView: 'PWA.view.main.Main'
 });
